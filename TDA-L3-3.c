3a95e2e23636701c0af267423e51e7f664352cd8
user-default <71413474+user-default@users.noreply.github.com>
2020-10-01 10:34:53 -0300

    TDA-L3-3.c

--- TDA-L3-3.c
+++ TDA-L3-3.c
@@ -8,7 +8,6 @@ A função strcat concatena o conteúdo de uma string em outra. DICA: Cuidado co
 
 int main()
 {
-  char s0[10] = "Ola ";
   char s1[] = " ";
   int i;
 
@@ -17,8 +16,7 @@ int main()
   printf("Digite seu nome:\n");
   for(;i != 0;)
   {
-    //printf("Digite seu nome:\n");
-    scanf("%s", s1);//No final do bloco o array s1 armazenava a ultima entrada na linha 27 e terminava por "passar por cima" do printf da linha 22. Solução: coloquei o printf na linha 19.
+    scanf("%s", s1);
     strcat(s1, ", como vai? ");
     printf("%s\n", s1);
     puts("Digite outro nome para repetir ou 0 para terminar.\n");
