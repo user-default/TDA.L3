ff0fd6a23d4df4a2f3e9b6676da8f15506b5753e
user-default <71413474+user-default@users.noreply.github.com>
2020-10-01 16:04:28 -0300

    TDA-L3-5.c

---
+++ TDA-L3-5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+
+/*
+Palíndromo é a frase ou palavra que se pode ler, indiferentemente, da esquerda para a direita ou vice-versa, como por exemplo a palavra ARARA ou o nome ANA.
+
+Escreva um programa que receba do usuário um texto e informe se é palíndromo ou não. Desconsidere caracteres em maíusculo, por exemplo, o nome Ana deve ser identificado como palíndromo.
+*/
+
+int main()
+{
+  char palavra[16] = "";
+  char palavra1[16] = "";
+  char palavra2[16] = "";
+  
+  puts("Digite uma palavra para verificar se ela e um palindromo:\n");
+  scanf("%s", palavra);
+  printf("OBS: A string possui %lu caracteres\n\n",strlen(palavra));
+
+  for(int i = 0; i < (strlen(palavra)); i++)
+  {
+    palavra1[i]= palavra[i];
+    printf("[%i]%c\t",i, palavra[i]);
+  }
+  puts("\n");
+  for(int i = (strlen(palavra)-1), j = 0;i >= 0; i-- && j++)
+  {
+    palavra2[j]= palavra[i];
+    printf("[%i]%c\t", j, palavra[i]);
+  }
+  if (strcasecmp(palavra1, palavra2) != 0) printf("\nA palavra %s nao e um palindromo!\n", palavra);
+  else printf("\nA palavra %s e um palindromo!\n", palavra);
+  //int resultado = (strcmp(palavra1, palavra2));
+  //printf("%d\n", resultado);
+  //puts(palavra);
+  //puts(palavra1);
+  //puts(palavra2);
+
+  return 0;
+}
