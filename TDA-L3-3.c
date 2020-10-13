7f46190cf382f0570ed517f1a26778088a6d2c2c
user-default <71413474+user-default@users.noreply.github.com>
2020-10-13 12:05:01 -0300

    TDA-L3-3.c

--- TDA-L3-3.c
+++ TDA-L3-3.c
@@ -1,28 +1,23 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 /*
 Implemente a função strcat de string.h utilizando qualquer estrutura de repetição. 
 A função strcat concatena o conteúdo de uma string em outra. DICA: Cuidado com o espaço em memória.
 */
 
-
 int main()
 {
-  char s1[] = " ";
-  int i;
-
-  puts("Digite qualquer valor exceto 0 para iniciar:\n");
-  scanf("%d", &i);
-  printf("Digite seu nome:\n");
-  for(;i != 0;)
-  {
-    scanf("%s", s1);
-    strcat(s1, ", como vai? ");
-    printf("%s\n", s1);
-    puts("Digite outro nome para repetir ou 0 para terminar.\n");
-    scanf("%d", &i);
-    if(i == 0) break;
-  }
-  puts("Programa encerrado!\n");
+    int i, len = 0;
+    char s1[100];
+    char s2[100];
+    scanf(" %[^\n]s", s1);
+    scanf(" %[^\n]s", s2); 
+    len = strlen(s1);
+    for(i = 0; i < strlen(s2); i++)
+    {
+      s1[len+i]= s2[i];
+      printf("%c\n", s1[len+i]);
+    }
+    puts(s1);
 }
